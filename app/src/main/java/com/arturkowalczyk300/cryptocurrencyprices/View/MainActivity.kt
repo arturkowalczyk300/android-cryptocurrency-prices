818bc09b5fb42bea4b8c41ee3c7fd6f994527f71
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-05-04 23:07:18 +0200

    add navigation through data records

--- app/src/main/java/com/arturkowalczyk300/cryptocurrencyprices/View/MainActivity.kt
+++ app/src/main/java/com/arturkowalczyk300/cryptocurrencyprices/View/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.arturkowalczyk300.cryptocurrencyprices.Model.Room.CryptocurrencyPricesDatabase
+import com.arturkowalczyk300.cryptocurrencyprices.Model.Room.CryptocurrencyPricesEntityDb
 import com.arturkowalczyk300.cryptocurrencyprices.R
 import com.arturkowalczyk300.cryptocurrencyprices.ViewModel.CryptocurrencyPricesViewModel
 import com.arturkowalczyk300.cryptocurrencyprices.ViewModel.CryptocurrencyPricesViewModelFactory
@@ -25,10 +26,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var etCurrencySymbol: EditText
     private lateinit var etDate: EditText
     private lateinit var btnGet: Button
-
+    private lateinit var btnPrevRecord: Button
+    private lateinit var btnNextRecord: Button
+    private lateinit var tvCurrentAndMaxIndex: TextView
     private lateinit var tvCryptocurrencySymbol: TextView
     private lateinit var tvCryptocurrencyDate: TextView
     private lateinit var tvCryptocurrencyPrice: TextView
+
+    private var currentRecordIndex: Int = 0
+    private var maxRecordIndex: Int = 0
+
+    private var listOfRecords: List<CryptocurrencyPricesEntityDb>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         etCurrencySymbol = findViewById(R.id.etCurrencySymbol)
         etDate = findViewById(R.id.etDate)
         btnGet = findViewById(R.id.btnGet)
+        btnPrevRecord = findViewById(R.id.btnPrevRecord)
+        btnNextRecord = findViewById(R.id.btnNextRecord)
+
+        tvCurrentAndMaxIndex = findViewById(R.id.tvCurrentAndMaxIndex)
 
         tvCryptocurrencySymbol = findViewById(R.id.tvCryptocurrencySymbol)
         tvCryptocurrencyDate = findViewById(R.id.tvCryptocurrencyDate)
@@ -57,27 +69,64 @@ class MainActivity : AppCompatActivity() {
                     etCurrencySymbol.text.toString(),
                     date
                 )
-                    /*.observe(this, Observer {
-                    if (it.entity != null) {
-                        Log.v("myApp", "response in MainActivity")
-                        //tvResponse.text = it.toString()
-
-                        tvCryptocurrencySymbol.text = it.currencySymbol
-                        tvCryptocurrencyDate.text = it.getFormattedDate()
-                        tvCryptocurrencyPrice.text =
-                            "%.3fUSD".format(it.entity?.market_data?.current_price?.usd)
-                    }
-                })*/
             } catch (exc: Exception) {
                 Log.v("myApp", exc.toString())
             }
         }
 
+        btnPrevRecord.setOnClickListener {
+
+            currentRecordIndex = (currentRecordIndex - 1)
+            if (currentRecordIndex < 0)
+                currentRecordIndex = maxRecordIndex
+            updateIndexInfo()
+            displayRecordByIndex(currentRecordIndex)
+        }
+
+        btnNextRecord.setOnClickListener {
+            currentRecordIndex = (currentRecordIndex + 1)
+            if (currentRecordIndex > maxRecordIndex)
+                currentRecordIndex = 0
+            updateIndexInfo()
+            displayRecordByIndex(currentRecordIndex)
+        }
+
         //observe all readings
         viewModel.getAllReadings()?.observe(this, Observer {
-            Log.v("myApp", "count: ${it.size}")
+            listOfRecords = it
+            maxRecordIndex = it.size - 1
+            updateIndexInfo()
         }
         )
+        updateIndexInfo()
+        displayRecordByIndex(currentRecordIndex)
+    }
+
+    fun updateIndexInfo() {
+        tvCurrentAndMaxIndex.text = "${currentRecordIndex + 1}/${maxRecordIndex + 1}"
+    }
+
+    fun displayRecordByIndex(index: Int) {
+        try {
+            val entity: CryptocurrencyPricesEntityDb? = listOfRecords?.get(currentRecordIndex)
+
+            if (entity != null) {
+                val sdf = SimpleDateFormat("dd.MM.yyyy")
+
+                tvCryptocurrencySymbol.text = entity.cryptocurrencyId
+                tvCryptocurrencyDate.text = sdf.format(entity.date)
+                tvCryptocurrencyPrice.text =
+                    "%.3fUSD".format(entity.priceUsd)
+            }
+            /*if (it.entity != null) {
+            Log.v("myApp", "response in MainActivity")
+            //tvResponse.text = it.toString()
+
+
+        }*/
+        } catch (exc: Exception) {
+            Log.e("myApp", exc.toString())
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -88,14 +137,16 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
 
-            R.id.itemAddRecord -> {
+            /*R.id.itemAddRecord -> {
                 //viewModel.addReading()
             }
             R.id.itemGetAllRecords -> {
                 viewModel.getAllReadings()
-            }
+            }*/
             R.id.itemClearRecords -> {
                 viewModel.clearAllRecords()
+                currentRecordIndex=0
+                maxRecordIndex =0
             }
         }
         return super.onOptionsItemSelected(item)
