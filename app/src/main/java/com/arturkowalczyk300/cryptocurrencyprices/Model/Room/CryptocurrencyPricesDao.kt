38e1d3730c07a8b51641426d4a1126d0aa59805f
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-05-04 22:01:34 +0200

    add database handling with basics operations

--- app/src/main/java/com/arturkowalczyk300/cryptocurrencyprices/Model/Room/CryptocurrencyPricesDao.kt
+++ app/src/main/java/com/arturkowalczyk300/cryptocurrencyprices/Model/Room/CryptocurrencyPricesDao.kt
@@ -1,4 +1,20 @@
 package com.arturkowalczyk300.cryptocurrencyprices.Model.Room
 
-class CryptocurrencyPricesDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+
+@Dao
+interface CryptocurrencyPricesDao {
+    @Insert(onConflict = IGNORE)
+    fun addReading(entity: CryptocurrencyPricesEntityDb)
+
+    @Query("SELECT * FROM main_table ORDER BY date DESC")
+    fun getAllReadings(): LiveData<List<CryptocurrencyPricesEntityDb>>
+
+    @Query("DELETE FROM main_table")
+    fun deleteAllReadings()
 }
--- app/src/main/java/com/arturkowalczyk300/cryptocurrencyprices/ViewModel/CryptocurrencyPricesViewModel.kt
+++ app/src/main/java/com/arturkowalczyk300/cryptocurrencyprices/ViewModel/CryptocurrencyPricesViewModel.kt
@@ -1,13 +1,16 @@
 package com.arturkowalczyk300.cryptocurrencyprices.ViewModel
 
+import android.app.Application
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.arturkowalczyk300.cryptocurrencyprices.Model.CryptocurrencyPricesRepository
+import com.arturkowalczyk300.cryptocurrencyprices.Model.Room.CryptocurrencyPricesEntityDb
 import com.arturkowalczyk300.cryptocurrencyprices.Model.WebAccess.RequestWithResponse
 import java.util.*
 
-class CryptocurrencyPricesViewModel : ViewModel() {
-    var repository: CryptocurrencyPricesRepository = CryptocurrencyPricesRepository()
+class CryptocurrencyPricesViewModel(application: Application) : ViewModel() {
+    var repository: CryptocurrencyPricesRepository = CryptocurrencyPricesRepository(application)
 
     fun requestPriceData(
         currencySymbol: String,
@@ -16,7 +19,27 @@ class CryptocurrencyPricesViewModel : ViewModel() {
         return repository.requestPriceData(currencySymbol, date)
     }
 
-    fun clearAllRecords(){
+    fun clearAllRecords() {
+        repository.clearAllRecords()
+    }
 
+    fun addReading() {
+        repository.addReading(
+            CryptocurrencyPricesEntityDb(
+                cryptocurrencyId = "dsczxa",
+                date = Date(2342),
+                priceUsd = 0.1223
+            )
+        )
+    }
+
+    fun getAllReadings() {
+        repository.getAllReadings()
+            ?.observeForever(androidx.lifecycle.Observer { t ->
+                t.forEach {
+                    //Log.v("myApp", "element")
+                }
+                Log.v("myApp", "count: ${t.size}")
+            })
     }
 }
