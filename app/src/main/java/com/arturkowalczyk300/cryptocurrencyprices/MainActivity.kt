3b4fe28714c35d4ba6ff419f54963efa7d375322
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-05-04 12:30:09 +0200

    init commit, add generated project

---
+++ app/src/main/java/com/arturkowalczyk300/cryptocurrencyprices/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.arturkowalczyk300.cryptocurrencyprices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
